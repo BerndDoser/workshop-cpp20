a7f5493a9f560e4ee1998bce8c7a313d81859764
Bernd Doser <bernd.doser@h-its.org>
2022-05-11 10:16:37 +0000

    add coroutine generator

---
+++ src/generator_iterator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class range
+{
+private:
+    int last;
+    int iter;
+
+public:
+    range(int end):
+        last(end),
+        iter(0)
+    {}
+
+    // Iterable functions
+    const range& begin() const { return *this; }
+    const range& end() const { return *this; }
+
+    // Iterator functions
+    bool operator!=(const range&) const { return iter < last; }
+    void operator++() { ++iter; }
+    int operator*() const { return iter; }
+};
+
+int main()
+{
+    for (int i: range(10))
+    {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
