9abc1683c78835ad2a4ddf830d691119a7ae1394
Bernd Doser <bernd.doser@h-its.org>
2022-05-18 08:32:07 +0000

    add range_laziness

---
+++ src/ranges_laziness.cpp
@@ -0,0 +1,21 @@
+#include <ranges>
+#include <iostream>
+ 
+int main()
+{
+    auto const data = {0,1,2,3,4,5};
+ 
+    // "pipe" syntax of composing the views:
+    auto result { data
+        | std::views::transform([](const auto& value) {
+            std::cout << "mult " << value << std::endl;
+            return value * 2;
+        })
+    };
+
+    std::cout << "here 1" << std::endl;
+    std::cout << result[0] << std::endl;
+
+    std::cout << "here 2" << std::endl;
+    std::cout << result[3] << std::endl;
+}
