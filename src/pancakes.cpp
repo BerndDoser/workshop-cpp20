5f7a8c7e98a1bb0ea49559fb528d8ff143c76fdf
Bernd Doser <bernd.doser@h-its.org>
2022-07-14 08:07:55 +0000

    pancakes: transform subrange

--- src/pancakes.cpp
+++ src/pancakes.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    //std::string stack{"-+-"};
+    //std::string stack{"+-+-"};
     //auto stack{1,0,1,0};
     std::vector<int> stack{1,0,1,0};
     auto first = std::ranges::find(stack, 0);
@@ -19,16 +19,15 @@ int main()
         else return 1;
     };
 
-    auto s2 = stack | std::views::transform([](const int i){ return 5; });
-    //std::cout << std::endl;
+    auto sub = std::ranges::subrange(std::begin(stack), first + 1);
+
+    //s2 = stack | std::views::transform([](const int i){ return 5; });
+    std::ranges::transform(sub, std::begin(sub), flip);
 
     //std::ranges::subrange(std::begin(stack), first + 1) |= std::views::transform(flip);
 
     std::ranges::for_each(stack, [](const int i){ std::cout << i; });
     std::cout << std::endl;
-
-    int n = 0;
-    std::cout << n << std::endl;
 
     return 0;
 }
