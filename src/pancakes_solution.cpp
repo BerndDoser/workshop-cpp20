a271710c4109873be05542db39180a2cb642a930
Bernd Doser <bernd.doser@h-its.org>
2022-07-14 11:33:30 +0000

    pancakes fix solution

--- src/pancakes_solution.cpp
+++ src/pancakes_solution.cpp
@@ -8,14 +8,11 @@
 // 2 +-
 // 0 +++
 // 3 --+-
-// ++--+-
+// 4 ++--+-
 
 int main()
 {
-    std::string stack{"+--"};
-    auto pos = std::ranges::find(std::views::reverse(stack), '-');
-    std::cout << *pos << std::endl;
-    std::cout << std::distance(std::rbegin(stack), pos) << std::endl;
+    std::string stack{"++--+-"};
 
     auto flip = [](const char c){
         return c == '+' ? '-' : '+';   // Conditional/Ternary operator
@@ -25,13 +22,12 @@ int main()
     for (auto first = std::ranges::find(stack, '-'); first != std::end(stack); first = std::ranges::find(stack, '-'), n++)
     {
         if (stack[0] == '+') {
-            auto sub = std::ranges::subrange(std::begin(stack), first + 1);
+            auto sub = std::ranges::subrange(std::begin(stack), first);
             std::ranges::transform(sub, std::begin(sub), flip);
         } else {
-            auto last_unhappy = std::ranges::find(std::views::reverse(stack), '-');
-            auto sub = std::ranges::subrange(std::begin(stack), first + 1);
+            auto first_happy = std::ranges::find(stack, '+');
+            auto sub = std::ranges::subrange(std::begin(stack), first_happy);
             std::ranges::transform(sub, std::begin(sub), flip);
-            std::ranges::reverse(sub);
         }
 
         std::ranges::for_each(stack, [](const char i){ std::cout << i; });
