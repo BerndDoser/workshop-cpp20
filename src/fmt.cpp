622ab1d24f32fe7217bf155801c83f296d758f1d
Bernd Doser <bernd.doser@h-its.org>
2022-07-18 11:00:33 +0000

    spaceship and format

--- src/fmt.cpp
+++ src/fmt.cpp
@@ -1,6 +1,29 @@
+#include <cassert>
 #include <fmt/core.h>
+#include <fmt/ranges.h>
+#include <iostream>
+#include <iomanip>
+#include <numbers>
+#include <vector>
 
 int main()
 {
     fmt::print("Hello, world!\n");
+
+    std::string s = fmt::format("{1}, {0}!", "world", "Hello", "something");
+    assert(s == "Hello, world!");
+    std::cout << s << std::endl;
+
+    std::vector<int> v = {1, 2, 3};
+    fmt::print("{}\n", v);
+
+    auto pi = std::numbers::pi_v<double>;  // C++20 introduce mathematical constants
+    std::cout << std::setprecision(2) << std::fixed << pi << std::endl;
+
+    fmt::print("{:.5f}\n", pi);
+
+    fmt::print("{:<30}\n", "left aligned");
+    fmt::print("{:>30}\n", "right aligned");
+    fmt::print("{:^30}\n", "centered");
+    fmt::print("{:*^30}\n", "centered");
 }
--- src/format.cpp
+++
@@ -1,6 +0,0 @@
-#include <format>
-
-int main()
-{
-    std::cout << std::format("Hello {}!\n", "world");
-}
