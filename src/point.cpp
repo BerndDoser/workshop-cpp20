00d0637ba0be504813940efdeb0b9ceef084b300
Bernd Doser <bernd.doser@h-its.org>
2022-07-19 08:25:17 +0000

    fix some missing links

--- src/point.cpp
+++ src/point.cpp
@@ -1,19 +1,43 @@
+#include <set>
 #include <cassert>
 
-class Point
+template<typename T1, typename T2>
+void TestComparisons(T1 a, T2 b)
 {
-public:
-    Point (int x, int y)
+    (a < b), (a <= b), (a > b), (a >= b), (a == b), (a != b);
+}
+
+struct Point
+{
+    Point(int x, int y)
      : x(x), y(y)
     {}
-    friend bool operator == (Point const& p1, Point const& p2) { return p1.x == p2.x && p1.y == p2.y; }
-private:
-    int x, y;
+
+    friend bool operator == (const Point& A, const Point& B) {
+        return true;
+    }
+
+    bool operator != (Point const& other) const {
+        return true;
+    }
+
+    int x;
+    int y;
 };
 
 int main()
 {
-    Point p1(1, 2);
-    Point p2(1, 1);
-    assert(p1 == p2);
+    Point P1(1,2);
+    Point P2(1,2);
+    Point P3(2,1);
+    Point P4(1,1);
+
+    TestComparisons(P1, P2);
+
+    assert(P1 == P2);
+    assert(P1 != P3);
+    assert(P4 < P1);
+
+    std::set<Point> points;
+    points.insert(Point(1,2));
 }
