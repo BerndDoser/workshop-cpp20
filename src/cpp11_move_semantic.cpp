6d3c0bcdaa455f7d861f30474e91f0655b79f114
Bernd Doser <bernd.doser@h-its.org>
2022-07-19 09:57:19 +0000

    Example move semantics

--- src/cpp11_move_semantic.cpp
+++ src/cpp11_move_semantic.cpp
@@ -1,7 +1,13 @@
 #include <type_traits>
+#include <utility>
+#include <iostream>
 
 struct Foo
 {
+    Foo(int i = 0) : i(i) {}
+    Foo(Foo& other) : i(other.i) { std::cout << "copy" << std::endl; }
+    Foo(Foo&& other) : i(std::exchange(other.i,0)) { std::cout << "move" << std::endl; }
+
     int i;
 };
 
@@ -11,6 +17,9 @@ int main()
     auto b = a;
     static_assert(std::is_same<decltype(a), int>::value);
 
-    Foo f1{a};  // Copy constructor
-    Foo f2{2};  // Move constructor
+    Foo f0(2);
+    Foo f1{f0};
+    Foo f2{std::move(f0)};
+    std::cout << f2.i << std::endl;
+    std::cout << f0.i << std::endl;
 }
